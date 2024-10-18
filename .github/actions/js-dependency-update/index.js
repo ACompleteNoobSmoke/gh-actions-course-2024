c8f9786090c6a4376488dc6d9e479f6172377814
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2024-10-18 07:18:37 -0500

    Testing

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,7 +1,17 @@
 const core = require('@actions/core');
 
 async function run() {
-    core.info("I am a custom JS action")
+    core.info("I am a custom JS action\n")
+    const branchName = core.getInput("base-branch");
+    const targetName = core.getInput("target-branch");
+    const workingDirectory = core.getInput("working-directory");
+    const debug = core.getBooleanInput("debug");
+
+    console.log(`Base Branch: ${branchName}`);
+    console.log(`Target Branch: ${targetName}`);
+    console.log(`Working Directory: ${workingDirectory}`);
+    console.log(`Debug: ${debug}`);
+
 }
 
 run();
